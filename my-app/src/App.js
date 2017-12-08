cc275a41897d092d5655e362d09b03442ccf382d
Julie Moon <8juliemoon@gmail.com>
2017-12-08 12:16:26 -0500

    passing props to Person

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1> testing </h1> 
-        <h2><Person/></h2>
+        <h2><Person name="Julie" age="32">I am short</Person></h2>
       </div>
     );
   }
