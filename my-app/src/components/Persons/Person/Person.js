b07ab405ecdfb7f56df4d060c4e546d101cf14b1
Julie Moon <8juliemoon@gmail.com>
2017-12-09 16:26:51 -0500

    made Person stateful component

--- my-app/src/components/Persons/Person/Person.js
+++ my-app/src/components/Persons/Person/Person.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Person.css';
 // access the click button from App.js by passing the switchNameHandler method as a prop
-const person = ( props ) => {
-  return (
-  <div className="Person">
-    <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-    <p onClick={props.click}>{props.children}</p>
-    <input type="text" onChange={props.changed} value={props.name}/>
-   </div>
-  )
-};
 
-export default person; 
+class Person extends Component{
+  render(){
+    return (
+      <div className="Person">
+        <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+        <p onClick={this.props.click}>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+       </div>
+      )
+  }
+}
+
+
+export default Person; 
